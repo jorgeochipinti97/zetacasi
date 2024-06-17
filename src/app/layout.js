7354f6132d7d760bbe42a9be4bb91462720374ec
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2024-06-17 05:04:38 -0300

    @'

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,8 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 export const metadata = {
   title: "Zeta Casino",
   description: "",
@@ -12,10 +13,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </Head>
       <body className={inter.className}>
         <Toaster />
         {children}
-        <Analytics/>
+        <Analytics />
       </body>
     </html>
   );
